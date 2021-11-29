d775f0c55ce47acb2a4e01615fd4fb24f95a84c7
kristellu <kristellu@uninorte.edu.co>
2021-11-28 20:50:44 -0500

    Added to do activity

--- lib/domain/controller/chat_controller.dart
+++ lib/domain/controller/chat_controller.dart
@@ -8,58 +8,24 @@ class ChatController extends GetxController {
   final databaseReference = FirebaseDatabase.instance.reference();
   var messages = <Message>[].obs;
 
-  start() {
-    messages.clear();
-    databaseReference
-        .child("fluttermessages")
-        .onChildAdded
-        .listen(_onEntryAdded);
+  //Método start para comenzar a escuchar la colección de documentos FlutterMessages,   
+  //que contiene el mensaje a enviar en los chats   
+  // start() {}
 
-    databaseReference
-        .child("fluttermessages")
-        .onChildChanged
-        .listen(_onEntryChanged);
-  }
+  //Método onEntryChanged listener  
+  //para añadir al listado los mensajes que llegan de la db
+  // _onEntryChanged(Event event) {}
 
-  _onEntryChanged(Event event) {
-    print("Something was changed");
-    var oldEntry = messages.singleWhere((entry) {
-      return entry.key == event.snapshot.key;
-    });
-    messages[messages.indexOf(oldEntry)] = Message.fromSnapshot(event.snapshot);
-  }
-
-  stop() {
-    databaseReference
-        .child("fluttermessages")
-        .onChildAdded
-        .listen(_onEntryAdded)
-        .cancel();
-
-    databaseReference
-        .child("fluttermessages")
-        .onChildChanged
-        .listen(_onEntryChanged)
-        .cancel();
-  }
+  //Método stop para dejar de escuchar la colección
+  // stop() {}
 
   _onEntryAdded(Event event) {
     print("Something was added");
     messages.add(Message.fromSnapshot(event.snapshot));
   }
 
-  Future<void> sendMsg(String text) async {
-    String uid = FirebaseAuth.instance.currentUser!.uid;
-    try {
-      databaseReference
-          .child("fluttermessages")
-          .push()
-          .set({'text': text, 'uid': uid});
-    } catch (error) {
-      logError("Error sending msg $error");
-      return Future.error(error);
-    }
-  }
+  //Método sendmsg para enviar un nuevo mensaje
+  // Future<void> sendMsg(String text) async {}
 
   Future<void> updateMsg(Message message) async {
     logInfo('updateMsg with key ${message.key}');
