cff3bad236e622285ae14cc892f67f6394dda48f
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2022-04-04 19:21:02 -0500

    Update configuration.dart

--- lib/config/configuration.dart
+++ lib/config/configuration.dart
@@ -1,11 +1,10 @@
 class Configuration {
-  static const apiKey = "AIzaSyAux8cWiT7fYtRd70pmNjeerda62cvhRio";
-  static const authDomain = "fluttermultifunction.firebaseapp.com";
-  static const databaseURL =
-      "https://fluttermultifunction-default-rtdb.firebaseio.com";
-  static const projectId = "fluttermultifunction";
-  static const storageBucket = "fluttermultifunction.appspot.com";
-  static const messagingSenderId = "348933724486";
-  static const appId = "1:348933724486:web:e7f4832e2509ce9730ad47";
-  static const measurementId = "G-JJV11VC7E8";
+  static const apiKey = "";
+  static const authDomain = "Your values";
+  static const databaseURL = "";
+  static const projectId = "Your values";
+  static const storageBucket = "Your values";
+  static const messagingSenderId = "Your values";
+  static const appId = "Your values";
+  static const measurementId = "";
 }
